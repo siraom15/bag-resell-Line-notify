d7efd3e5fd409e4343e1bdaffde77811d8549863
Áom | Siriwat J <siriwatjaiyungyuen@gmail.com>
2022-02-06 07:37:39 +0700

    Fix Cron time

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ import cron from 'node-cron';
 import { getDataAndSend } from './helper/message.js';
 
 // send in every hour
-cron.schedule('* */1 * * *', async () => {
+cron.schedule('0 0 */1 * * *', async () => {
   getDataAndSend();
 });
