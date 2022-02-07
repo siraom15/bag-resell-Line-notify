90ce80d55e57d86859f056da855d8a6e66e66a0e
siraom15 <siriwatjaiyungyuen@gmail.com>
2022-02-07 13:48:53 +0700

    update

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import cron from 'node-cron';
 import { getDataAndSend } from './helper/message.js';
 
 // work every 30 mins
+console.info('App Start');
 cron.schedule('0 */30 * * * *', async () => {
+  console.info('Cron Start');
   getDataAndSend();
 });
